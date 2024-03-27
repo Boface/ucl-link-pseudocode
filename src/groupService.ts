d4255bfb4b7377d8a202e25fb9a915666b6bb77a
Boface <bofacebl@hotmail.com>
2024-03-27 02:56:06 +0000

    small fix

--- src/groupService.ts
+++ src/groupService.ts
@@ -1,4 +1,4 @@
-import { GroupInterface, GroupChatInterface } from './interfaces';
+import { GroupInterface, GroupChatInterface } from '../common/interfaces';
 
 export class GroupService implements GroupInterface {
   // Implement group-related functionality
